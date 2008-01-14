095472fa5e0859a112504688bb6629cce9f876e8
zipleen <zipleen@gmail.com>
2008-01-14 23:23:26 +0000

    items done.

--- src/items_vida.cpp
+++ src/items_vida.cpp
@@ -15,37 +15,37 @@ Items_Vida::Items_Vida(GLfloat x,GLfloat y,int valor)
 	switch(valor){
 		case 15:
 		#ifdef WIN32
-			this->item_path="data\\models\\items\\life\\small_cross.md3";
-			this->item_skin_path="data\\models\\items\\life\\gold.tga";
+			this->item_path="data\\models\\items\\life\\medikit.md3";
+			this->item_skin_path="data\\models\\items\\life\\medikit3.tga";
 		#else
-			this->item_path="data/models/items/life/small_cross.md3";
-			this->item_skin_path="data/models/items/life/gold.tga";
+			this->item_path="data/models/items/life/medikit.md3";
+			this->item_skin_path="data/models/items/life/medikit3.tga";
 		#endif
 			this->item_code = 4001;
-			this->mesh_name = "";		
+			this->mesh_name = "key";		
 			break;
 		case 25:
 		#ifdef WIN32
-			this->item_path="data\\models\\items\\life\\medium_cross.md3";
-			this->item_skin_path="data\\models\\items\\life\\gold.tga";
+			this->item_path="data\\models\\items\\life\\medikit.md3";
+			this->item_skin_path="data\\models\\items\\life\\medikit2.tga";
 		#else
-			this->item_path="data/models/items/life/medium_cross.md3";
-			this->item_skin_path="data/models/items/life/gold.tga";
+			this->item_path="data/models/items/life/medikit.md3";
+			this->item_skin_path="data/models/items/life/medikit2.tga";
 		#endif
 			this->item_code = 4002;
-			this->mesh_name = "";		
+			this->mesh_name = "key";		
 			break;		
 			break;
 		case 50:
 		#ifdef WIN32
-			this->item_path="data\\models\\items\\life\\large_cross.md3";
-			this->item_skin_path="data\\models\\items\\life\\gold.tga";
+			this->item_path="data\\models\\items\\life\\medikit.md3";
+			this->item_skin_path="data\\models\\items\\life\\medikit1.tga";
 		#else
-			this->item_path="data/models/items/life/large_cross.md3";
-			this->item_skin_path="data/models/items/life/gold.tga";
+			this->item_path="data/models/items/life/medikit.md3";
+			this->item_skin_path="data/models/items/life/medikit1.tga";
 		#endif
 			this->item_code = 4003;
-			this->mesh_name = "";		
+			this->mesh_name = "key";		
 			break;			
 			break;
 	
